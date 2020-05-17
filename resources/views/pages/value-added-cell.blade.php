59cdf1260f1b80f108c74b2ab5b881ef7fdf2d36
Bhaktij Koli <bhaktijkoli121@gmail.com>
2020-05-17 10:05:15 +0530

    Updated Value Added Cell

--- resources/views/pages/value-added-cell.blade.php
+++ resources/views/pages/value-added-cell.blade.php
@@ -11,26 +11,24 @@
       <div class="separator"></div>
       <br>
       <div class="row object-non-visible" data-animation-effect="fadeIn">
-        <div class="col-md-12">
-          @foreach (App\Event::where('url', 'vac')->orderBy('date','desc')->get() as $event)
-            <div class="col-sm-6 col-md-3">
-              <div class="image-box">
-                <div class="overlay-container">
-                  <img src="{{url("images/eclipse.gif")}}" alt="" data-echo="{{$event->getFeaturedImage()}}" style="height: 250px;margin: 0 auto;" height="500">
-                  <a class="overlay" href="{{route('event', $event->url)}}">
-                    <i class="fa fa-search-plus"></i>
-                    <span>{{App\Department::getName($event->department)}}</span>
-                  </a>
-                </div>
-                <a class="btn btn-default btn-block" href="{{route('event', $event->url)}}">
-                  <p style="overflow: hidden; margin: 0px; white-space: normal;" class="ellipsis_3_line">
-                    {{$event->name}}
-                  </p>
+        @foreach (App\Event::where('department', 'vac')->orderBy('date','desc')->get() as $event)
+          <div class="col-sm-6 col-md-3">
+            <div class="image-box">
+              <div class="overlay-container">
+                <img src="{{url("images/eclipse.gif")}}" alt="" data-echo="{{$event->getFeaturedImage()}}" style="height: 250px;margin: 0 auto;" height="500">
+                <a class="overlay" href="{{route('event', $event->url)}}">
+                  <i class="fa fa-search-plus"></i>
+                  <span>{{App\Department::getName($event->department)}}</span>
                 </a>
               </div>
+              <a class="btn btn-default btn-block" href="{{route('event', $event->url)}}">
+                <p style="overflow: hidden; margin: 0px; white-space: normal;" class="ellipsis_3_line">
+                  {{$event->name}}
+                </p>
+              </a>
             </div>
-          @endforeach
-        </div>
+          </div>
+        @endforeach
       </div>
     </div>
   </div>
