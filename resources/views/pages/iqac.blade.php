7550aaf32fefbcce9102c7f1104ccdddebf0a937
Bhaktij Koli <bhaktijkoli121@gmail.com>
2021-06-26 00:07:25 +0530

    chore: updated iqac meetings

--- resources/views/pages/iqac.blade.php
+++ resources/views/pages/iqac.blade.php
@@ -187,7 +187,7 @@ $menu_item = 'iqac';
 				<div id="meetings" class="{{$action=='meetings'?'tab-pane fade in active':'tab-pane fade in'}}">
 					<h3>Meetings</h3>
 					<ul>
-						@for ($i=1; $i < 14+1; $i++)
+						@for ($i=1; $i < 18+1; $i++)
 						<li><a target="_blank" href="{{route('iqac-meetings',$i)}}">Meeting {{$i}}</a></li>
 						@endfor
 					</ul>
